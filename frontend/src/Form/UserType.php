376e95e2f27b44dcec09062fa12e32f9dee0124c
michalm57 <mroczekmichal57@gmail.com>
2025-11-21 08:49:34 +0100

    feat(frontend): Add User creation and editing forms

---
+++ frontend/src/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('first_name', TextType::class, ['label' => 'First name'])
+            ->add('last_name', TextType::class, ['label' => 'Last name'])
+            ->add('gender', ChoiceType::class, [
+                'label' => 'Gender',
+                'choices' => [
+                    'Male' => 'male',
+                    'Female' => 'female',
+                ],
+            ])
+            ->add('birthdate', DateType::class, [
+                'label' => 'Birthdate',
+                'widget' => 'single_text',
+                'input' => 'string',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => null,
+        ]);
+    }
+}
